5b9e8fa1229873f3a2703f782062f8a69f59e019
Vincent Leclerc <vleclerc.dev@gmail.com>
2021-11-01 23:33:02 +0100

    add a default error layout

---
+++ layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="flex m-2 p-2 rounded-md bg-red-300 ring-2 ring-red-600 text-red-600"
+  >
+    <h1>An error occurred!</h1>
+    &nbsp;
+    <nuxt-link class="hover:underline" to="/">Go back to home.</nuxt-link>
+  </div>
+</template>
